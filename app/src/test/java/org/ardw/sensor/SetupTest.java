2d4becf5ea16648641a7b466bbae564859661eda
llschall <llschall@gmx.com>
2024-07-27 19:12:32 +0200

    [Build] Add Ardwloop 0.1.3

--- app/src/test/java/org/ardw/sensor/SetupTest.java
+++ app/src/test/java/org/ardw/sensor/SetupTest.java
@@ -3,11 +3,19 @@ package org.ardw.sensor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import javafx.application.Application;
+import org.llschall.ardwloop.ArdwloopStarter;
 
 public class SetupTest {
 
     @Test
     public void checkJavaFx() {
         Assertions.assertFalse(Application.class.getName().isBlank());
+    }
+
+
+    @Test
+    public void checkArdwloop() {
+        Assertions.assertEquals("0.1.3", ArdwloopStarter.VERSION);
+        Assertions.assertEquals(1001, ArdwloopStarter.VERSION_INT);
     }
 }
